1391a2e5ea74903c49606e6a50dcc75d2b69bb01
johnedisc <christopher.johnedis@gmail.com>
2023-02-07 12:48:29 -0800

    refactor index.js to import clas from another file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
+import {WeatherService} from './js/weatherService.js';
 import './css/styles.css';
-
-// Business Logic
-
-function getWeather(city) {
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-    request.addEventListener("loadend", function() {
-      const response = JSON.parse(this.responseText);
-      if (this.status === 200) {
-        resolve([response, city]);
-      } else {
-        reject([this, response, city]);
-      }
-    });
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise.then(function(response) {
-    printElements(response);
-  }, function(errorMessage) {
-    printError(errorMessage);
-  });
-}
 
 // UI Logic
 
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  let weatherPromise = WeatherService.getWeather(city);
+  weatherPromise.then((weatherDataArr) => {
+    printElements(weatherDataArr);
+  }, (errorArr) => {
+    printError(errorArr);
+  });
+}
+
 function printElements(results) {
-  console.log(results);
   document.querySelector('#showResponse').innerText = `The humidity in ${results[1]} is ${results[0].main.humidity}%.
   The temperature in Kelvins is ${results[0].main.temp} degrees.`;
 }
 
 function printError(error) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
-}
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
 }
 
 window.addEventListener("load", function() {
